c019dabecb2ec8a7ba68199d29dc5c877573b68c
Adam O'Neill <adamo.mark25@gmail.com>
2022-01-23 17:06:58 +0000

    Data mined from website

---
+++ webscrape.py
@@ -0,0 +1,71 @@
+#---------------------------------------------------------------------#
+# File: c:\Adam\Coding\HLStockUpdate\webscrape.py
+# Project: c:\Adam\Coding\HLStockUpdate
+# Created Date: Sunday, January 23rd 2022, 3:58:35 pm
+# Description: 
+# Author: Adam O'Neill
+# Copyright (c) 2022 Adam O'Neill
+# -----
+# Last Modified: Sun Jan 23 2022
+# Modified By: Adam O'Neill
+# -----
+# HISTORY:
+# Date      	By	Comments
+# ----------	---	---------------------------------------------------------
+#---------------------------------------------------------------------#
+ 
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+def webScrape (my_url):
+    # Opens connection to page
+    uClient = uReq(my_url)
+
+    # Page contents into variable
+    page_html = uClient.read()
+
+    # Closes connection
+    uClient.close()
+
+    # html parsing into soup
+    page_soup = soup(page_html, "html.parser")
+
+
+    # grab tables, We want second table [1] as this contains info we want
+    info_tables = page_soup.findAll("table", {"class":"factsheet-table"})
+    info_table = info_tables[1]
+
+    # grabs table body
+    info_table_contents = info_table.tbody
+
+    # Creates list of all the table rows
+    table_rows = info_table_contents.findAll("tr")
+
+    # Loops through all the rows
+    for row in table_rows:
+
+        # Creates list of all the table d within the current row
+        table_d = row.findAll("td")
+
+
+        # Creates temp list for holding each row data to be put into 2d array
+        temp_row_list = []
+
+        # Loops through each <td>
+        for d in table_d:
+            current_text = d.text
+            temp_row_list.append(current_text)
+        
+        # Appends the temp_row_list to page data list
+        page_data_list.append(temp_row_list)
+
+
+def main():
+    currentUrl = "https://www.hl.co.uk/funds/fund-discounts,-prices--and--factsheets/search-results/j/jpm-uk-smaller-companies-class-c-accumulation/fund-analysis"
+    
+    # Desired table from the currentUrl is stored in the 2d array page_data_list
+    webScrape(currentUrl)
+    print(page_data_list)
+
+page_data_list = []
+main()
